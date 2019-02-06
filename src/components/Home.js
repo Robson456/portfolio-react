7114ca6b4ecf92e0cd970a3295f3f546e5c0a8df
Pacman4fun <kellherwos14@wp.pl>
2019-02-06 20:59:20 +0100

    fixing netlify form

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,6 @@ export default class Home extends Component {
   render() {
     return (
       <div className="">
-          <h2 className="black-text center">Hello there general kenobi</h2> 
           <h4 className="black-text center">Hello there, My name is Robert Przepiórka </h4>
           <h5 className="black-text center">And I'm front-end developer you need </h5>
 
