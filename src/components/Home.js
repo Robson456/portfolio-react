59eb5d39066b3c1ae439d4feb68ca252878d27d4
Pacman4fun <kellherwos14@wp.pl>
2019-02-10 04:16:21 +0100

    update about, and fonts, png to webp

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Bg from '../bg2.jpeg'
+import Bg from '../img/bg2.webp'
 import Projects from './Projects'
 
 export default class Home extends Component {
@@ -7,7 +7,7 @@ export default class Home extends Component {
     return (
       <div className="">
           <h4 className="black-text center">Hello there, My name is Robert Przepiórka </h4>
-          <h5 className="black-text center">And I'm front-end developer you need </h5>
+          <h5 className="black-text center">And I'm frontend developer you need </h5>
 
             <img src={Bg} alt="" className="responsive-img z-depth-2 "  style={{marginTop:'2rem'}}/>
           <Projects/>
--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class ThankYou extends Component {
   render() {
     return (
       <div className="center"> 
-        <h4 className="center" style={{marginTop:'5rem'}}>Thank You!</h4>
+        <h3 style={{marginTop:'5rem'}}>Thank You!</h3>
+        <Link to="/" className="black-text" style={{fontSize:'1.5rem'}}>Click here to go to Home page </Link>
+        
       </div>
     )
   }
